faaa629c41f00ad1e35aab566c6e35a8a130538c
saeid <saeid.noormohammad@gmail.com>
2022-03-06 18:02:27 +0300

    go

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,8 +1,6 @@
 import { ApolloServer } from "apollo-server-micro";
 import Cors from "micro-cors";
 import { pool } from "../../db/index";
-// import resolverAdmin from "../../graphql/admin/admin_resolver";
-// import typeAdmin from "../../graphql/admin/admin_type";
 import typeUser from "../../graphql/user/user_type";
 import resolverUser from "../../graphql/user/user_resolver";
 import typeProduct from "../../graphql/product/typeProduct";
