978e160c696f41e340e03ee3ec6156f00985b47b
saeid <saeid.noormohammad@gmail.com>
2022-03-27 16:21:17 +0300

    d

--- Modals/SendEmail/SendEmail.js
+++ Modals/SendEmail/SendEmail.js
@@ -1,17 +1,23 @@
 import React from "react";
 import sendEmail from "./sendEmail.module.css";
+import { useRouter } from "next/router";
 
 export default function SendEmail({ openM, setOpenM }) {
+  const router = useRouter();
   if (!openM) return null;
   return (
     <div className={sendEmail.overlay}>
       <div className={sendEmail.mainContainer}>
         <div className={sendEmail.holder}>
-          <p>
-            We just send an email to {openM} for veifiying your account check
-            your inbox!
-          </p>
-          {/* <button className={sendEmail.holderBtn}>Send Email Again</button> */}
+          <p>We just send an email to</p>
+          <p className={sendEmail.email}>{openM}</p>
+          <p>for veifiying your account check your inbox!</p>
+          <button
+            className={sendEmail.holderBtn}
+            onClick={() => router.push("/")}
+          >
+            Ok
+          </button>
         </div>
       </div>
     </div>
