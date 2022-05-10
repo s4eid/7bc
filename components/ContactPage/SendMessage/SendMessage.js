61e1f7544674f9e9be4bb1fb6fa2d69068a3938f
saeid <saeid.noormohammad@gmail.com>
2022-05-10 14:35:17 +0300

    go

--- components/ContactPage/SendMessage/SendMessage.js
+++ components/ContactPage/SendMessage/SendMessage.js
@@ -5,7 +5,7 @@ import sendMessage from "./sendMessage.module.css";
 export default function SendMessage() {
   return (
     <div className={sendMessage.mainC}>
-      <h1 className={sendMessage.title}>SendMessage</h1>
+      <h1 className={sendMessage.title}>Send Message</h1>
       <SendForm />
     </div>
   );
