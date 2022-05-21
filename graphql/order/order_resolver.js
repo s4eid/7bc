b4358b2ba71df0469e459ea52f5eaf5394c967ba
saeid <saeid.noormohammad@gmail.com>
2022-05-21 15:34:16 +0300

    d

--- graphql/order/order_resolver.js
+++ graphql/order/order_resolver.js
@@ -8,19 +8,19 @@ import { getSession } from "next-auth/react";
 const resolverUser = {
   Query: {
     async getOrder(_, { order_id }, { pool, user, req }) {
-      let _user;
-      const googleUser = await getSession({ req });
-      _user = googleUser?.user ? googleUser.user : null;
-      if (!user && !_user) {
-        return new ApolloError("You Are Not Authenticated!", ERROR_CODES.AUTH);
-      }
+      // let _user;
+      // const googleUser = await getSession({ req });
+      // _user = googleUser?.user ? googleUser.user : null;
+      // if (!user && !_user) {
+      //   return new ApolloError("You Are Not Authenticated!", ERROR_CODES.AUTH);
+      // }
       const data = await getOrder(order_id, pool);
       return data;
     },
     async getOrders(_, { user_id }, { pool, user, _user }) {
-      if (!user && !_user) {
-        return new ApolloError("You Are Not Authenticated!", ERROR_CODES.AUTH);
-      }
+      // if (!user && !_user) {
+      //   return new ApolloError("You Are Not Authenticated!", ERROR_CODES.AUTH);
+      // }
       const data = await getOrders(user_id, pool);
       return data;
     },
@@ -50,9 +50,9 @@ const resolverUser = {
       },
       { pool, user, _user }
     ) {
-      if (!user && !_user) {
-        return new ApolloError("You Are Not Authenticated!", ERROR_CODES.AUTH);
-      }
+      // if (!user && !_user) {
+      //   return new ApolloError("You Are Not Authenticated!", ERROR_CODES.AUTH);
+      // }
       const data = await add_order(
         user_id,
         email,
