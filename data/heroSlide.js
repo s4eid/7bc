e74547074c8d0af3d8372bd242261dde79bc031f
saeid <saeid.noormohammad@gmail.com>
2022-05-25 17:14:31 +0300

    d

--- data/heroSlide.js
+++ data/heroSlide.js
@@ -1,17 +1,17 @@
 export const slideItems = [
   {
     name: "Carpet",
-    link: "/hero.jpg",
-    id: "1",
+    link: "/hero_1.jpg",
+    id: "3",
   },
+  // {
+  //   name: "Carpet",
+  //   link: "/hero.jpg",
+  //   id: "1",
+  // },
   {
     name: "Carpet",
     link: "/slide-overlay.jpg",
     id: "2",
-  },
-  {
-    name: "Carpet",
-    link: "/hero_1.jpg",
-    id: "3",
   },
 ];
